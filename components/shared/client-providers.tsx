c3e0bf9f1758fe53f7cc857683ea14bc0d255808
Souhail <i79916475@gmail.com>
2025-01-16 15:40:59 +0100

    Initial commit

---
+++ components/shared/client-providers.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react'
+import useCartSidebar from '@/hooks/use-cart-sidebar'
+import CartSidebar from './cart-sidebar'
+import { Toaster } from '../ui/toaster'
+
+
+export default function ClientProviders({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const isCartSidebarOpen = useCartSidebar()
+
+  return (
+    <>
+
+    {isCartSidebarOpen ? ( 
+        <div className='flex minh-screen'>
+           <div className='flex-1 overflow-hidden'>{children}</div>
+           <CartSidebar />
+        </div>
+      ) : (
+        <div>{children}</div>
+    )}
+    <Toaster />
+
+    </>
+  )
+}
---
+++ components/shared/footer.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { ChevronUp } from 'lucide-react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { APP_NAME } from '@/lib/constants'
+
+export default function Footer() {
+  return (
+    <footer className='bg-black  text-white underline-link'>
+      <div className='w-full'>
+        <Button
+          variant='ghost'
+          className='bg-gray-800 w-full  rounded-none '
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          <ChevronUp className='mr-2 h-4 w-4' />
+          Back to top
+        </Button>
+      </div>
+      <div className='p-4'>
+        <div className='flex justify-center  gap-3 text-sm'>
+          <Link href='/page/conditions-of-use'>Conditions of Use</Link>
+          <Link href='/page/privacy-policy'> Privacy Notice</Link>
+          <Link href='/page/help'>Help</Link>
+        </div>
+        <div className='flex justify-center text-sm'>
+          <p> © 2015-2025, {APP_NAME}, Inc. or its affiliates</p>
+        </div>
+        <div className='mt-8 flex justify-center text-sm text-gray-400'>
+          Hay Riad, Bab Louh, Rabat, Morroco, 50040 | +212 6 45 46 78 90
+        </div>
+      </div>
+    </footer>
+  )
+}
