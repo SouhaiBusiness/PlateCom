85b4c32d0782492055dbb641e5a9b7c59e3802ee
SouhaiBusiness <i79916475@gmail.com>
2025-07-06 18:30:19 +0100

    fixing mongodb

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,16 @@
-import { Geist, Geist_Mono } from 'next/font/google'
+import { GeistSans, GeistMono } from 'geist/font'
 import './globals.css'
 import ClientProviders from '@/components/shared/client-providers'
 import { getDirection } from '@/i18n-config'
-//import { NextIntlClientProvider } from 'next-intl'
-//import { getMessages } from 'next-intl/server'
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/constants'
 import { Metadata } from 'next'
-//import { routing } from '@/i18n/routing'
- 
-//import { getSetting } from '@/lib/actions/setting.actions'
- 
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
-
-    title: {
-      template: `%s | ${APP_NAME}`,
-      default: `${APP_NAME}. ${APP_SLOGAN}`,
-    },
-    description: APP_DESCRIPTION,
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: `${APP_NAME}. ${APP_SLOGAN}`,
+  },
+  description: APP_DESCRIPTION,
 }
 
 export default async function AppLayout({
@@ -37,29 +20,19 @@ export default async function AppLayout({
   params: { locale: string }
   children: React.ReactNode
 }) {
-   
-   
- 
-  const { locale } = await params
-  // Ensure that the incoming `locale` is valid
-  
- 
- // const messages = await getMessages()
+  const { locale } = params
 
   return (
     <html
       lang={locale}
       dir={getDirection(locale) === 'rtl' ? 'rtl' : 'ltr'}
       suppressHydrationWarning
+      className={`${GeistSans.variable} ${GeistMono.variable}`}
     >
-      <body
-        className={`min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        
-          <ClientProviders>
-            {children}
-          </ClientProviders>
-       
+      <body className="min-h-screen antialiased">
+        <ClientProviders>
+          {children}
+        </ClientProviders>
       </body>
     </html>
   )
